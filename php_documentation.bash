0c876a11f00109f479af0d0a843c91ca9a5c8dca
OsrKozuki <oscar.alor@iteso.mx>
2022-07-19 14:38:55 -0500

    php learn

---
+++ php_documentation.bash
@@ -0,0 +1,15 @@
+!! Documentación de PHP !!
+ Ejecucion de cliente
+    - JavScript
+    - VBSCript
+    - Applets Java
+    - HTML
+    - CSS
+
+Ejecucion del lado del servidor
+    - PHP
+    - JavScript
+    - Perl
+    - ASP  
+
+!! Programación en PHP !!
