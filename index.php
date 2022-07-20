7eaa48d6f5703a850fdacd66b68d1dfb26de82e2
OsrKozuki <oscar.alor@iteso.mx>
2022-07-19 19:12:57 -0500

    php learn

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"
+    <title>Learn PHP</title>
+</head>
+<body>
+
+
+<?php
+
+    print "Aprendiendo PHP <br>";
+    print "Hola mundo <br>";
+    print "Ya no se que mas hacer xd <br>";
+
+?>
+
+
+</body>
+
+</html>
